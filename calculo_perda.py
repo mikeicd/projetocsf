635cd34b10b1f4c10c7b6a6163ca74beb5ce2234
Mike Domingues <mikeicd@gmail.com>
2023-06-26 17:34:07 -0300

    Testes do calculo de minimização do parametro N

--- calculo_perda.py
+++ calculo_perda.py
@@ -1,17 +1,26 @@
 import numpy as np
-from scipy.optimize import curve_fit
+import pandas as pd
+from scipy.optimize import curve_fit, minimize
 
 
-def perda_caminho(distancias, perdas, d0, pl_d0, N):
+def perda_caminho(distancias, perdas, d0, pl_d0):
     def modelo(distancias, N):
-        return pl_d0 + 10 * N * np.log10(distancias/d0)
+        return pl_d0 + (10 * N * np.log10(np.divide(distancias,d0)))
 
-    p0 = N  # Valor inicial para o expoente N
-    parametros, _ = curve_fit(modelo, distancias, perdas, p0=p0)
+    def loss_function(params):
+        y_pred = modelo(distancias, *params)
+        mse = np.mean((perdas - y_pred)**2)
+        return mse   
+    
+    p0 = 2  # Valor inicial para o expoente N
+    # parametros, _, infodict, errmsg, ier = curve_fit(modelo, distancias, perdas, p0=p0,full_output=True)
+    result = minimize(loss_function, p0, method='Nelder-Mead')
 
-    return parametros[0]
+    
+    return result
 
 
 
-dist = [40, 20, 30, 40, 50]
-perd = []
+# df = pd.read_json("./data/coordenadas.json")
+# print(df.head())
+
