58a08c6c077f48fde112f2248a5a0a1255f0dba3
Mike Domingues <mikeicd@gmail.com>
2023-06-26 17:33:07 -0300

    Melhorias no tratamento de dados.

--- main.py
+++ main.py
@@ -3,42 +3,44 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from calculo_perda import perda_caminho
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 files = os.listdir(f'{basedir}/data')
 
+PotTx = 14
+
 df_list = []
 for file in files:
-    print(file)
-    df = pd.read_csv("data/"+file)
-    df = df.groupby('Date and Time').first()
+    if file.endswith('.csv'):
+        df = pd.read_csv("data/"+file)
+        df = df.groupby('Date and Time').first()
+        df['Nome'] = file[:-4]
 
-    df_list.append(df)
-    print(df.head())
+        df_list.append(df)
+
+df_coord = pd.read_json("./data/coordenadas.json")
+
+df_total = pd.DataFrame(columns=['Nome','RSSI','Distc','Lat','Long'])
 
 for df in df_list:
-    print(df['rssi'].mean())
     
+    df_teste = pd.DataFrame()    
+    df_teste['Distc'] = df_coord['distância'].loc[df_coord['local'] == df['Nome'][0]]
+    df_teste['Lat'] = df_coord['latitude'].loc[df_coord['local'] == df['Nome'][0]]
+    df_teste['Long'] = df_coord['longitude'].loc[df_coord['local'] == df['Nome'][0]]
+    df_teste['Nome'] = df['Nome'][0]
+    df_teste['RSSI'] = df['rssi'].mean()
+    df_teste['Prx'] = PotTx + df['rssi'].mean() *-1
 
-locais = {}
+    df_total = pd.concat([df_total, df_teste])
 
-# for item in files:
-#     locais[item[:-4]] = {}
-#     print(item)
-#     with open(f'{basedir}/data/{item}', encoding="utf8") as csv_file:
-#         csv_data = csv.reader(csv_file, delimiter=',')
-#         lista = []
-#         first_line = True
-#         for row in csv_data:
-#             if not first_line:
-#                 lista.append(float(row[1]))
+df_total.reset_index(inplace=True,drop=True)
+print(df_total.head(100))
 
-#             first_line = False
-#         locais[item[:-4]]['rssi'] = lista
+parametro = perda_caminho(distancias=df_total['Distc'].to_list(),perdas=df_total['Prx'].to_list(), d0=10, pl_d0=(PotTx + -60.0)*-1)
 
-# for key, value in locais.items():
-#     print(key)
-#     value['max'] = np.max(value['medidas'])
-#     value['min'] = np.min(value['medidas'])
-#     value['mean'] = np.mean(value['medidas'])
+print(parametro)
+
+
