94d95bb91bc9f9ccc8dcf300f9adf0f236e1f6ed
Mike Domingues <mikeicd@gmail.com>
2023-06-30 19:37:22 -0300

    Update config mapa

--- mapa.py
+++ mapa.py
@@ -3,7 +3,7 @@ from folium.plugins import HeatMap
 import pandas as pd
 
 
-def cria_mapa(df:pd.DataFrame):
+def cria_mapa(df: pd.DataFrame):
 
     # Criar um mapa com base em uma localização inicial
     mapa = folium.Map(location=[-27.608355, -48.633345], zoom_start=15)
@@ -16,12 +16,21 @@ def cria_mapa(df:pd.DataFrame):
     for indice, dado in df.iterrows():
         lat = dado['Lat']
         lon = dado['Long']
-        pot = dado['RSSI']
+        pot = (dado['RSSI']-df['RSSI'].min()) / \
+            (df['RSSI'].max() - df['RSSI'].min())
+        print(pot)
         coordenadas.append([lat, lon, pot])
         i += 1
 
     # Criação do objeto HeatMap com as coordenadas
-    heatmap = HeatMap(coordenadas)
+    heatmap = HeatMap(coordenadas,min_opacity=0.1, blur=1, radius=50, gradient={
+        '0.2': 'deepskyblue',
+        '0.4': 'aqua',
+        '0.6': 'lime',
+        '0.8': 'gold',
+        '0.9': 'orange',
+        '1.0': 'red'
+    })
     # Adicionar o mapa de calor ao mapa
     heatmap.add_to(mapa)
 
